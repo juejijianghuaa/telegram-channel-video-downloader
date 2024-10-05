ac63391d17e7eada44a01b3963a176fbbc7c1029
juejijianghuaa <494173758@qq.com>
2024-10-05 09:21:52 +0000

    tgsearch的docker版，对应amd64或arm64平台运行，解压后使用sh rundocker.sh或sh rundocker.arm64.sh运行，会自动导入image。运行前需要手工修改docker-compose.yml或docker-compose.arm64.yml文件，把session和proxy放入。（api_id,api_hash,proxy可省略) 讨论组:  https://t.me/pandagroovechat 发布频道: https://t.me/PandaGroovePG
    今日更新：
    1.tgsearch支持/s/路径搜索，可以直接免登录搜索tg频道，而且支持了缓存和分页，并支持海报展示。
    2.优化异步并发效率，提高了多个群组和频道的搜索速度。
    3.忽略ssl错误。
    4.TG搜索改用POST方法
    5.修复TG免登录搜索结果中的时间错误问题。

--- pg/rundocker.sh
+++ pg/rundocker.sh
@@ -15,7 +15,7 @@ done
 progdir=`dirname "${prog}"`
 cd "${progdir}"
 
-version=1.2.3-alpine
+version=1.2.4-alpine
 
 found=$(docker images |grep "pandagroove/tgsearch:${version}" |wc -l)
 
--- pg/stopdocker.sh
+++ pg/stopdocker.sh
@@ -15,7 +15,7 @@ done
 progdir=`dirname "${prog}"`
 cd "${progdir}"
 
-version=1.2.3-alpine
+version=1.2.4-alpine
 
 compose=""
 have=$(docker compose -h |grep compose|wc -l)
